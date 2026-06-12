971ce75ea5272cb72d59c1736cfcb764bd1dd978
Khaomi <46065349+Khaomi@users.noreply.github.com>
2026-06-12 16:57:08 +0700

    feat: more progress with quadborg

--- Content.Shared/Radio/Components/EncryptionKeyHolderComponent.cs
+++ Content.Shared/Radio/Components/EncryptionKeyHolderComponent.cs
@@ -46,6 +46,7 @@ public sealed partial class EncryptionKeyHolderComponent : Component
     ///     Combined set of radio channels provided by all contained keys.
     /// </summary>
     [ViewVariables]
+    [DataField("channels")] // Hardlight
     public HashSet<string> Channels = new();
 
     /// <summary>
