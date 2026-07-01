3b405006858d41ab557e1533cf7358226c402dd7
DevArchwave <168038123+DevArchwave@users.noreply.github.com>
2026-07-01 03:49:56 -0300

    Removal of previous system

--- Content.Client/Options/UI/Tabs/MiscTab.xaml.cs
+++ Content.Client/Options/UI/Tabs/MiscTab.xaml.cs
@@ -81,8 +81,6 @@ public sealed partial class MiscTab : Control
         Control.AddOptionCheckBox(CCVars.ChatFancyNameBackground, FancyNameBackgroundsCheckBox);
         Control.AddOptionCheckBox(CCVars.StaticStorageUI, StaticStorageUI);
         Control.AddOptionCheckBox(MonoCVars.RadioNoiseEnabled, RadioNoiseCheckBox); // Mono
-        Control.AddOptionCheckBox(MonoCVars.AreaEchoEnabled, AreaEchoCheckBox); // Mono
-        Control.AddOptionCheckBox(MonoCVars.AreaEchoHighResolution, AreaEchoHighResolutionCheckBox); // Mono
 
         Control.Initialize();
     }
